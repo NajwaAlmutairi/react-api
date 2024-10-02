e570cfffa238c3176da5edefab2b09a7fa88c70b
najwa <NajwaBuayjan@gmail.com>
2024-10-02 12:11:19 +0300

    solving the lab

--- reactapi/src/App.jsx
+++ reactapi/src/App.jsx
@@ -19,7 +19,7 @@ function App() {
   return (
     <>
     <div className='bg-gray-200'>
-      <h1 className="text-3xl font-bold text-center my-6">Our Products</h1>
+      <h1 className="text-3xl font-bold text-center py-6">Our Products</h1>
       <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 w-[90%] mx-auto'>
         {products.map((ele, index) => {
           return (
